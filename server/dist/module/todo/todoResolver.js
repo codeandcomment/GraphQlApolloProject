c02cbed302264130bc1c9bc10a9f7877bd2636a9
JB <infinityjaithra@gmail.com>
2025-03-04 12:11:55 +0530

    Added Schema Resolver and CRUD methods

---
+++ server/dist/module/todo/todoResolver.js
@@ -0,0 +1,53 @@
+import prisma from "../../config/database.js";
+const todoResolver = {
+    Query: {
+        todos: async () => {
+            return await prisma.todo.findMany({ orderBy: { id: "desc" } });
+        },
+        getTodo: async (__dirname, { id }) => {
+            return await prisma.todo.findUnique({ where: { id: id } });
+        }
+    },
+    Mutation: {
+        createTodo: async (_, { todo }) => {
+            const newTodo = await prisma.todo.create({
+                data: {
+                    todo: todo,
+                    completed: false
+                }
+            });
+            return newTodo;
+        },
+        updateTodo: async (_, { id, todo }) => {
+            await prisma.todo.update({
+                data: {
+                    todo: todo
+                },
+                where: {
+                    id: id
+                }
+            });
+            return { message: "Todo Updated Successfully" };
+        },
+        toggleComplete: async (_, { id, data }) => {
+            await prisma.todo.update({
+                data: {
+                    completed: data
+                },
+                where: {
+                    id: id
+                }
+            });
+            return { message: "Todo Updated Successfully" };
+        },
+        deleteTodo: async (_, { id }) => {
+            await prisma.todo.delete({
+                where: {
+                    id: id
+                }
+            });
+            return { message: "Todo Deleted Successfully" };
+        }
+    }
+};
+export default todoResolver;
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,13 +1,31 @@
 import express, {Application,Request,Response} from "express";
 import "dotenv/config"
+import appoloServer from "./config/apolloServer.js";
+import {expressMiddleware} from "@apollo/server/express4"
+import cors from "cors"
+
 
 const app:Application = express();
 
 const PORT = process.env.PORT || 7000;
 
+app.use(express.json())
+app.use(express.urlencoded({extended:false}))
+app.use(cors())
+
+
 app.get("/",(req:Request,res:Response)=>{
     return res.json({"ststus":200, "message": "App is up"})
 })
+
+const startApolloServer = async()=>{
+
+    await appoloServer.start();
+    app.use("/graphql",expressMiddleware(appoloServer))
+}
+
+
+startApolloServer()
 
 app.listen(PORT,()=>{
     console.log('Server is up on', PORT)
