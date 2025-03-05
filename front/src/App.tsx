7983cdef4cb9faac7edb68f5d892c1d94dca493b
JB <infinityjaithra@gmail.com>
2025-03-05 11:34:42 +0530

    Added Taildwind and configured, setup basic css html sekeletion

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,35 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React,{useRef} from 'react'
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+
+  const inputRef = useRef(null);
+  const handleSubmit = (event:React.FormEvent) =>{
+    event.preventDefault()
+    console.log(inputRef.current.value);
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="h-screen flex justify-center items-center">
+      <div className='w-full px-2 md:w-[600px] rounded-md bg-blue-100 h-[60vh]'>
+      <h1 className='text-xl font-bold my-2'> Todo App</h1>
+
+      <form onSubmit={handleSubmit}>
+          <input className='w-full h-10 p-2 outline-none rounded-md bg-white'
+          placeholder="Enter Todo here" ref={inputRef}></input>
+      </form>
+
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
-
-export default App
